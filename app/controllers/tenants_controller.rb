897b0128a7fb206b5b541c32e52d23b23bd15222
Pete Fowler <fowler.pete@gmail.com>
2022-10-18 14:05:31 -0600

    Complete

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,2 +1,39 @@
 class TenantsController < ApplicationController
+  def index 
+    tenants = Tenant.all
+    render json: tenants
+  end 
+
+  def show 
+    tenant = find_tenant
+    render json: tenant
+  end 
+
+  def create 
+    tenant = Tenant.create!(tenant_params)
+    render json: tenant, status: :created
+  end
+
+  def update
+    tenant = find_tenant
+    tenant.update!(tenant_params)
+    render json: tenant
+  end
+
+  def destroy
+    tenant = find_tenant
+    tenant.delete
+    render json: {}, status: :accepted
+  end
+
+  private 
+
+  def find_tenant
+    Tenant.find(params[:id])
+  end
+
+  def tenant_params 
+    params.permit(:name, :age)
+  end 
+
 end
