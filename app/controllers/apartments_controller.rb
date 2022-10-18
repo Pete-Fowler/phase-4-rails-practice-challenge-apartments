4ba2300bf3ec7fc357cc117bc736514ecbfc73ea
Pete Fowler <fowler.pete@gmail.com>
2022-10-18 13:30:46 -0600

    Add show to apartment

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -4,4 +4,15 @@ class ApartmentsController < ApplicationController
     render json: apts
   end 
 
+  def show 
+    apt = Apartment.find_by(apt_params)
+    render json: apt 
+  end 
+
+  private 
+
+  def apt_params 
+    params.permit(:id, :number)
+  end 
+
 end
